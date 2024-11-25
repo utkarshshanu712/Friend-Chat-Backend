8d0edea7dba0d5b5f166a57f03b88c99295280e2
utkarshshanu712 <utkasrshanu712@gmail.com>
2024-11-25 21:08:42 +0530

    change1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://your-netlify-app.netlify.app"],
+    origin: ["http://localhost:5173", "https://chat220.netlify.app"],
     methods: ["GET", "POST"]
   }
 });
